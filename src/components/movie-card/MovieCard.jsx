0ede619a6654f04812a4069666768a627fe7f4c7
Asliddin Mirakhmedov <asliddin@MacBook-Pro-Asliddin.local>
2022-09-11 23:20:09 +0500

    react-movie

---
+++ src/components/movie-card/MovieCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './movie-card.scss';
+
+import { Link } from 'react-router-dom';
+
+import Button from '../button/Button';
+
+import { category } from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
+
+const MovieCard = props => {
+    const item  = props.item;
+
+    const link = '/' + category[props.category] + '/' + item.id;
+
+    const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path);
+
+    const film = () => {
+        if(item.first_air_date) {
+            return item.first_air_date.split('-').splice(0, 1)
+        } else if(item.release_date){
+            return item.release_date.split('-').splice(0, 1)
+        } else {
+            return ""
+        }
+    }
+    // console.log(item);;
+
+    return (
+        <Link to={link}>
+            <div className="movie-card" style={{backgroundImage: `url(${bg})`}}>
+                <Button>
+                    <i className="bx bx-play"></i>
+                </Button>
+            </div>
+            <h3 style={{fontSize: "15px"}}>{item.title || item.name} ({film()})</h3>
+        </Link>
+    );
+}
+
+export default MovieCard
---
+++ src/pages/detail/VideoList.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import { useParams } from "react-router";
+
+import tmdbApi from "../../api/tmdbApi";
+
+const VideoList = (props) => {
+  const { category } = useParams();
+
+  const [videos, setVideos] = useState([]);
+  const [trailerKey, setTrailerKey] = useState("");
+  const [trailerName, setTrailerName] = useState("");
+
+  const [noTrailer, setNoTrailer] = useState(true);
+
+  useEffect(() => {
+    const getVideos = async () => {
+      const res = await tmdbApi.getVideos(category, props.id, {
+        params: {},
+      });
+      setVideos(
+        res.results.slice(0, 100).forEach((video) => {
+          if (video.name == "Official Trailer" || video.official == true) {
+            setTrailerKey(video.key);
+            setTrailerName(video.name);
+          } else if (
+            video.name == false ||
+            video.official == false
+          ) {
+            setNoTrailer(false);
+          }
+        })
+      );
+    };
+    getVideos();
+  }, [category, props.id]);
+
+  console.log(trailerKey + " " + trailerName);
+  return (
+    <>
+      {noTrailer ? (
+        ""
+      ) : (
+        <Video trailerKey={trailerKey} name={trailerName} />
+      )}
+    </>
+  );
+};
+
+const Video = ({ trailerKey, name }) => {
+  const iframeRef = useRef();
+
+  useEffect(() => {
+    const height = (iframeRef.current.offsetWidth * 9) / 16 + "px";
+    iframeRef.current.setAttribute("height", height);
+  }, []);
+
+  return (
+    <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+      <div className="video__title">
+        <h2>{name}</h2>
+      </div>
+      <iframe
+        src={`https://www.youtube.com/embed/${trailerKey}`}
+        ref={iframeRef}
+        width="1000px"
+        title="video"
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoList;
